996c66a80cfa6335f6e0dbab00a7d57eb569f862
Wellington Domiciano <wldomiciano@gmail.com>
2018-07-09 15:12:10 -0300

    Mudei a solução de thumbnails do blog. Ao invés de usar um <img>, estou usando imagens como background de uma <div>

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,14 +5,14 @@ var menu = document.getElementById("menu");
 var toggle2 = document.getElementById("toggle2");
 var menu2 = document.getElementById("menu2");
 
-toggle.onclick = function() {
+if (toggle) toggle.onclick = function() {
     if ( menu.classList.contains("expanded") )
         menu.classList.remove("expanded");
     else
         menu.classList.add("expanded");
 }
 
-toggle2.onclick = function() {
+if (toggle2) toggle2.onclick = function() {
     if ( menu2.classList.contains("expanded") )
         menu2.classList.remove("expanded");
     else
