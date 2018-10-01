3096e0e78c757b2fd8c885e40c7b799b6a9996dc
Wellington Domiciano <wldomiciano@gmail.com>
2018-10-01 20:24:40 -0300

    Adicionar compress.html
    Um template Jekyll usado para comprimir HTML

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ const del = require('del');
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const exec = require('child_process').exec;
-
+const htmlmin = require('gulp-htmlmin');
 
 
 const server = browserSync.create();
 const clean = () => del(['_site']);
+
+function minify(done) {
+  return gulp.src('_site/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+}
 
 function jekyll(done) {
   return exec('bundle exec jekyll build');
@@ -18,12 +24,11 @@ function reload(done) {
 }
 
 function serve(done) {
-  server.init({ server: { baseDir: '_site' } });
+  server.init({ server: { baseDir: 'dist' } });
   done();
 }
 
-const watch = () => gulp.watch(
-  ['**/*', '!_site'], gulp.series(jekyll, reload));
+const watch = () => gulp.watch(['**/*', '!_site'], gulp.series(jekyll, reload));
 const prod = gulp.series(clean, jekyll);
 const dev = gulp.series(clean, jekyll, serve, watch);
 
